6ee383b9d5953e44d1dc00195c133ea330ad5197
oliver.phillips <oliver.phillips@ticketmaster.co.uk>
2017-08-10 17:04:39 +0100

    Hook up browser and TCP server

--- server.js
+++ server.js
@@ -5,27 +5,32 @@ const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
-})
+});
+
+const client = new net.Socket();
+client.connect(6000, '10.40.72.142');
+
+client.on('data', (data) => {
+  const msg = data.toString().replace('ECHO: ', '');
+  console.log(`Joel message: ${msg}`);
+  io.emit('message', msg);
+});
+
+client.on('close', () => {
+  console.log('Closed :(');
+});
 
 io.on('connection', (socket) => {
   console.log('Connected');
+  socket.on('message', (msg) => {
+    console.log(`Client message: ${msg}`);
+    client.write(`${msg}\n`);
+  })
+  socket.on('disconnect', () => {
+    console.log('Disconnected');
+  })
 })
 
 http.listen(3000, () => {
   console.log('Listening on http://localhost:3000');
 })
-
-const client = new net.Socket();
-client.connect(6000, '10.40.72.142', () => {
-  console.log('Connected');
-});
-
-client.on('data', (data) => {
-  console.log(`Received: ${data}`);
-});
-
-client.on('close', () => {
-  console.log('Closed :(');
-})
-
-client.write('Hello again');
