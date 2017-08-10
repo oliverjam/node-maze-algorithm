da03696d5bbcc5325e774d9603613226dc169ce2
oliver.phillips <oliver.phillips@ticketmaster.co.uk>
2017-08-10 15:53:31 +0100

    Integrate Socket.io

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 const net = require('net');
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
+
+io.on('connection', (socket) => {
+  console.log('Connected');
+})
+
+http.listen(3000, () => {
+  console.log('Listening on http://localhost:3000');
+})
 
 const client = new net.Socket();
 client.connect(6000, '10.40.72.142', () => {
